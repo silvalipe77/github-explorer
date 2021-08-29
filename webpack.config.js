16573c06963733ba754482e3b7fce68152a3b078
Lipe <silvalipe77@hotmail.com>
2021-08-29 10:09:14 -0300

    configuraçãp

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPLugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public', 'index.html')
+  },
+  plugins: [
+    new HtmlWebpackPLugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
