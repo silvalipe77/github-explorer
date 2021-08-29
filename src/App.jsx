c89e8585dc39106135ab861b8c3449a9203264c8
Lipe <silvalipe77@hotmail.com>
2021-08-29 16:30:12 -0300

    add webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
   
-  return <RepositoryList/>
+  return (
+    
+  <RepositoryList/>
+  )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,25 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+import { useState, useEffect } from "react";
 
 const repository= {
   name: 'uniform',
   description: 'form in react',
   link:'https://github.com'
 };
+// https://api.github.com/users/silvalipe77/repos
 
 export function RepositoryList(){
+  const [repositories,setRepositories] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users/silvalipe77/repos')
+    .then(response=>response.json)
+    .then(data=>console.log(data))
+
+  },[])
+
+
   return(
     <section className="repository-list">
       <h1>lista de repositorios</h1>
