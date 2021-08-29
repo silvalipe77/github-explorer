087e2cf0e080daab8a780a220bcf8f4aae43d3e0
Lipe <silvalipe77@hotmail.com>
2021-08-29 12:04:43 -0300

    criando componentes

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+export function RepositoryList(){
+  return(
+    <section className="repository-list">
+      <h1>lista de repositorios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+          <a href="">
+            Acessar repositorio
+          </a>
+        </li>
+      </ul>
+
+    </section>
+  )
+}
